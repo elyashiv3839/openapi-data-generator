f73876481f2b371f1441d45bb8588d39eb7d85f9
Elyashiv Danino <elyashiv3839@gmail.com>
2022-01-03 09:38:24 +0200

    Fix release bug

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="openapi-data-generator",
-    version="1.0.1",
+    version="1.0.2",
     author="elyashiv3839, RAZALKALY",
     author_email="elyashiv3839@gmail.com, RAZALKALY@YAHOO.COM",
-    description="Resolve schema and deploy to single schema",
+    description="Generate all valid permutations and invalid permutations - according OpenAPI specification",
     long_description="file: README.md",
     long_description_content_type="text/markdown",
     url="https://github.com/elyashiv3839/openapi-data-generator.git",
-    download_url="https://github.com/elyashiv3839/openapi-data-generator/archive/refs/tags/1.0.1.tar.gz",
+    download_url="https://github.com/elyashiv3839/openapi-data-generator/archive/refs/tags/1.0.2.tar.gz",
     project_urls={"Bug Tracker": "https://github.com/elyashiv3839/openapi-data-generator.git/issues"},
     classifier=[
         "Programming Language :: Python :: 3",
