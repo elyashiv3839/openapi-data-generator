b9d979b2c41e95b26b510e8860cf9a02887c8018
Elyashiv Danino <elyashiv3839@gmail.com>
2022-01-03 09:42:31 +0200

    Update description - new release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [v1.0.2]
 ### Added
+- Update description.
+
+## [v1.0.2]
+### Added
 - Fix build bug.
 
 ## [v1.0.0]
--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .openapi_data_generator.OpenApiDataGenerator import OpenApiDataGenerator
 
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __author__ = "elyashiv3839, RAZALKALAY"
