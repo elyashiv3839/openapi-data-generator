6edcab3dfa5e9393bfe68571cdeb56d3bc242f95
elyashiv3839 <61381172+elyashiv3839@users.noreply.github.com>
2022-03-01 11:01:53 +0200

    Release 1.0.4

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="openapi-data-generator",
-    version="1.0.3",
+    version="1.0.4",
     author="elyashiv3839, RAZALKALY",
     author_email="elyashiv3839@gmail.com, RAZALKALY@YAHOO.COM",
     description="Generate all valid and invalid permutations of payloads - according OpenAPI specification",
     long_description="file: README.md",
     long_description_content_type="text/markdown",
     url="https://github.com/elyashiv3839/openapi-data-generator.git",
-    download_url="https://github.com/elyashiv3839/openapi-data-generator/archive/refs/tags/1.0.3.tar.gz",
+    download_url="https://github.com/elyashiv3839/openapi-data-generator/archive/refs/tags/1.0.4.tar.gz",
     project_urls={"Bug Tracker": "https://github.com/elyashiv3839/openapi-data-generator.git/issues"},
     classifier=[
         "Programming Language :: Python :: 3",
